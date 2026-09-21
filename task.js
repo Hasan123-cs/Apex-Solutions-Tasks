5721a09735569840c08b057e04ee91509bf26ae5
Hasan Jako <HasanJako06@gmail.com>
2026-09-21 16:47:12 +0300

    finish : task 1-2 adding the error on the field require && adding the edit feature

--- task.js
+++ task.js
@@ -1,5 +1,7 @@
 let timer;
 let timeLeft;
+// what mean ? when click edit the editingUserId contain the id 1234-asncds...
+let editingUserId = null;
 const savedUsers = localStorage.getItem("users");
 let users = [];
 const form = document.getElementById("MyForm");
@@ -9,6 +11,8 @@ const Gender = document.getElementById("selectedGender");
 const saveBtn = document.getElementById("saveBtn");
 const userTable = document.getElementById("userTable");
 const timerElement = document.getElementById("timer");
+const firstNameError = document.getElementById("firstNameError");
+const lastNameError = document.getElementById("lastNameError");
 if (savedUsers) {
   users = JSON.parse(savedUsers);
   displayUsers();
@@ -20,18 +24,55 @@ function saveUsers() {
 form.addEventListener("submit", function (event) {
   // for no refresh un page
   event.preventDefault();
-  users.push({
-    id: crypto.randomUUID(),
-    firstName: firstName.value,
-    lastName: lastName.value,
-    gender: Gender.value,
-  });
+  const firstNameValue = firstName.value.trim();
+  const lastNameValue = lastName.value.trim();
+  let isValid = true;
+
+  if (firstNameValue.length < 3) {
+    firstNameError.textContent = "First name must be at least 3 characters";
+    isValid = false;
+  } else {
+    firstNameError.textContent = "";
+  }
+
+  if (lastNameValue.length < 3) {
+    lastNameError.textContent = "Last name must be at least 3 characters";
+    isValid = false;
+  } else {
+    lastNameError.textContent = "";
+  }
+  // stop the page if its invalid
+  if (!isValid) {
+    return;
+  }
+  // now push dosent work like the old since we have 2 state update and create so based on the id
+  // remembeerd we can deduce where we are
+  if (editingUserId === null) {
+    // here no update its create state so do the old
+    users.push({
+      id: crypto.randomUUID(),
+      firstName: firstNameValue,
+      lastName: lastNameValue,
+      gender: Gender.value,
+    });
+  } else {
+    // here its an update and we know the id so lets find user and update
+    const user = users.find(function (user) {
+      return user.id === editingUserId;
+    });
+
+    user.firstName = firstNameValue;
+    user.lastName = lastNameValue;
+    user.gender = Gender.value;
+  }
+
   saveUsers();
   startTimer();
   displayUsers();
-  // to clear form for reusable the form
+  // reset the resources
+  editingUserId = null;
+  saveBtn.textContent = "Save";
   form.reset();
-  // disable save-btn  again
   saveBtn.disabled = true;
 });
 
@@ -68,11 +109,15 @@ function displayUsers() {
                 <td>${user.firstName}</td>
                 <td>${user.lastName}</td>
                 <td>${user.gender}</td>
-                <td>
+                <td style="display: flex; gap: 10px;">
                     <button onclick="deleteUser('${user.id}')">
                         Delete
                     </button>
+                    <button onclick="editUser('${user.id}')">
+                          Edit
+                    </button>
                 </td>
+                
             </tr>
         `;
   });
@@ -104,3 +149,18 @@ function startTimer() {
 }
 
 startTimer();
+function editUser(id) {
+  const user = users.find(function (user) {
+    return user.id === id;
+  });
+  if (!user) {
+    return;
+  }
+  firstName.value = user.firstName;
+  lastName.value = user.lastName;
+  Gender.value = user.gender;
+  // remember who edit
+  editingUserId = id;
+  saveBtn.textContent = "Update";
+  saveBtn.disabled = false;
+}
