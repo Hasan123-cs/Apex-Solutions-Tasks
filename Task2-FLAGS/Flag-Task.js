6f7916eec04fad8875864b29d69c6323dea8e5ed
Hasan Jako <HasanJako06@gmail.com>
2026-09-23 19:53:45 +0300

    Add favorites filter with localStorage support

--- Task2-FLAGS/Flag-Task.js
+++ Task2-FLAGS/Flag-Task.js
@@ -5,9 +5,28 @@ const API_URL = "https://api.restcountries.com/countries/v5";
 let countries = [];
 
 let currentPage = 1;
-
+let showFavoritesOnly = false;
 const rowsPerPage = 5;
+let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+// events listener
+document.getElementById("closeModal").addEventListener("click", () => {
+  document.getElementById("countryModal").style.display = "none";
+});
+document.getElementById("countryModal").addEventListener("click", (e) => {
+  if (e.target.id === "countryModal") {
+    e.target.style.display = "none";
+  }
+});
+document.getElementById("showFavorites").addEventListener("change", () => {
+  showFavoritesOnly = document.getElementById("showFavorites").checked;
 
+  currentPage = 1;
+
+  displayCountries();
+});
+
+// ===events listener===
+// handlers
 function getCountries() {
   let requests = [];
 
@@ -35,41 +54,93 @@ function getCountries() {
       console.log(error);
     });
 }
-
 function displayCountries() {
   const table = document.getElementById("countryTable");
 
   table.innerHTML = "";
-
+  let filteredCountries = countries;
+  if (showFavoritesOnly) {
+    filteredCountries = countries.filter((country) => {
+      return isFavorite(country);
+    });
+  }
+  const totalPages = Math.ceil(filteredCountries.length / rowsPerPage);
+  if (currentPage > totalPages && totalPages > 0) {
+    currentPage = totalPages;
+  }
   const startIndex = (currentPage - 1) * rowsPerPage;
-
   const endIndex = startIndex + rowsPerPage;
+  const pageCountries = filteredCountries.slice(startIndex, endIndex);
+  if (pageCountries.length === 0) {
+    table.innerHTML = `
+      <tr>
+        <td colspan="4">
+          No favorite countries found
+        </td>
+      </tr>
+    `;
 
-  const pageCountries = countries.slice(startIndex, endIndex);
-  const totalPages = Math.ceil(countries.length / rowsPerPage);
+    document.getElementById("pageNumber").innerText = "0 of 0";
+
+    return;
+  }
+
   pageCountries.forEach((country) => {
     const row = document.createElement("tr");
+
+    row.addEventListener("click", () => {
+      openCountryModal(country);
+    });
 
     if (country.population < 5000000) {
       row.style.backgroundColor = "red";
     } else {
-      row.style.backgroundColor = "limegreen";
+      row.style.backgroundColor = "#adff2f";
     }
 
     row.innerHTML = `
-<td>
-<img src="${country.flag.url_png}" width="50">
-</td>
-<td>
-${country.names.common}
-</td>
-<td>
-${country.population.toLocaleString()}
-</td>
-<td>
-</td>
 
-`;
+    <td>
+      <img src="${country.flag.url_png}" width="50">
+    </td>
+
+
+    <td>
+      ${country.names.common}
+    </td>
+
+
+    <td>
+      ${country.population.toLocaleString()}
+    </td>
+
+
+    <td>
+
+      <button class="favorite-btn"
+      style="
+      ${
+        isFavorite(country)
+          ? "background-color: gold; color:white;"
+          : "background-color:lightgray; color:black;"
+      }
+      ">
+
+      ${isFavorite(country) ? "★ Favorited" : "☆ Favorite"}
+
+      </button>
+
+    </td>
+
+    `;
+
+    const favoriteBtn = row.querySelector(".favorite-btn");
+
+    favoriteBtn.addEventListener("click", (e) => {
+      e.stopPropagation();
+
+      addFavorite(country, favoriteBtn);
+    });
 
     table.appendChild(row);
   });
@@ -77,7 +148,6 @@ ${country.population.toLocaleString()}
   document.getElementById("pageNumber").innerText =
     `${currentPage} of ${totalPages}`;
 }
-
 document.getElementById("next").addEventListener("click", () => {
   const totalPages = Math.ceil(countries.length / rowsPerPage);
 
@@ -101,5 +171,62 @@ document.getElementById("prev").addEventListener("click", () => {
     document.getElementById("prev").disabled = true;
   }
 });
+function openCountryModal(country) {
+  // show modal
+  document.getElementById("countryModal").style.display = "flex";
 
+  // Flag
+  document.getElementById("modalFlag").src = country.flag.url_png || "";
+
+  // Name
+  document.getElementById("modalName").innerText =
+    country.names?.common || "N/A";
+
+  // Capital
+  document.getElementById("modalCapital").innerText =
+    country.capitals?.[0]?.name || "N/A";
+
+  // Population
+  document.getElementById("modalPopulation").innerText = country.population
+    ? country.population.toLocaleString()
+    : "N/A";
+
+  // Region
+  document.getElementById("modalRegion").innerText = country.region || "N/A";
+
+  // Subregion
+  document.getElementById("modalSubregion").innerText =
+    country.subregion || "N/A";
+
+  // Languages
+  document.getElementById("modalLanguages").innerText = country.languages
+    ? country.languages.map((lang) => lang.name).join(", ")
+    : "N/A";
+}
+// add favorite
+function addFavorite(country, button) {
+  const index = favorites.findIndex(
+    (fav) => fav.names.common === country.names.common,
+  );
+
+  if (index === -1) {
+    favorites.push(country);
+
+    button.innerHTML = "★ Favorited";
+
+    button.style.backgroundColor = "gold";
+    button.style.color = "white";
+  } else {
+    favorites.splice(index, 1);
+    button.innerHTML = "☆ Favorite";
+    button.style.backgroundColor = "lightgray";
+    button.style.color = "black";
+  }
+
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+}
+function isFavorite(country) {
+  return favorites.some((fav) => fav.names.common === country.names.common);
+}
+// ==handlers===
 getCountries();
